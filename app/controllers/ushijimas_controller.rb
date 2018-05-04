a35de79f0f0a5b9e7afc70b6c44638e4e367c633
Moe Onose <onose.moe.infratop@gmail.com>
2018-05-05 03:19:00 +0900

    修正

--- app/controllers/ushijimas_controller.rb
+++ app/controllers/ushijimas_controller.rb
@@ -1,7 +1,12 @@
 class UshijimasController < ApplicationController
+	# before_action :forbid_login_user, {only: [:top]}
   def top
+  	@user = User.new
+
   end
 
-  def ushijima
+  def show
+  	@event = Event.find(params[:id])
+  	# @users = @event.event_users
   end
 end
