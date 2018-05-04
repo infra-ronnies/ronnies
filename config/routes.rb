f2e46ca39c7ca6957472025613f11314cdcba916
Moe Onose <onose.moe.infratop@gmail.com>
2018-05-04 17:47:05 +0900

    ルーティング！！！！

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  get 'comments/create'
+    resources :users, only: [:show] do
+    	post 'create'
+    	post 'update'
+    end
 
-  get 'posts/show'
+    resources :events, only: [:new, :create, :edit, :update] do
+     resources :posts, only: [:show, :index, :edit, :update, :create, :destroy] do
+     	resources :post_comments, only: [:create, :destroy]
+     end
+    end
 
-  get 'posts/edit'
+    resources :ushijimas, only: [:show] do
+    	get 'top'
+    end
 
-  get 'events/new'
 
-  get 'events/edit'
 
-  get 'ushijimas/top'
-
-  get 'ushijimas/ushijima'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
