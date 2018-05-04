4a595701448a6debf6042a639faed43f644bdae1
tatsuki1116 <tatsuki1617@gmail.com>
2018-05-04 09:11:05 +0000

    index書きました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
+  def index
+  	@post = Post.new
+  end
+
   def show
   end
 
   def edit
   end
+
+private
+    def post_params
+        params.require(:post).permit(:title)
+    end
 end
