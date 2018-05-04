9d2e2ac25c21be9f7ff2d42a660c72175f57ee0e
ebinayuu <ebina.yuu.infratop@gmail.com>
2018-05-04 11:15:51 +0000

    イベント作成の編集

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,11 @@
 class EventsController < ApplicationController
   def new
+  	@event = Event.new
+  end
+  def create
   	event = Event.new(event_params)
   	event.save
-  	redirect_to event_path(event.id)
+  	redirect_to event_posts_path(event.id)
   end
 
   def edit
@@ -12,5 +15,9 @@ class EventsController < ApplicationController
   	event = Event.find(params[:id])
   	event.update(event_params)
   	redirect_to event_path(event.id)
+  end
+  private
+  def event_params
+  	params.require(:event).permit(:event_name, :image, :event_password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180504103707) do
-
 
   create_table "event_users", force: :cascade do |t|
     t.integer "event_id"
