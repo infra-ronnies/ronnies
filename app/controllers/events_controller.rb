de4c3b3fd69ff576f89f681123a69ba66e1d5d2f
ebinayuu <ebina.yuu.infratop@gmail.com>
2018-05-04 10:12:16 +0000

    イベント作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,9 @@ class EventsController < ApplicationController
   def edit
   	@event = Event.find(params[:id])
   end
+  def update
+  	event = Event.find(params[:id])
+  	event.update(event_params)
+  	redirect_to event_path(event.id)
+  end
 end
