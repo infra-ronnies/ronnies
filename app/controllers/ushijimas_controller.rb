5ab1782f2d76576fac39730dcec446f081fab462
Moe Onose <onose.moe.infratop@gmail.com>
2018-05-05 17:52:39 +0900

    kansei

--- app/controllers/ushijimas_controller.rb
+++ app/controllers/ushijimas_controller.rb
@@ -10,9 +10,8 @@ class UshijimasController < ApplicationController
 	def show
 		@event = Event.find(params[:id])
 		@posts = Post.where(event_id: @event.id).order(post_date: :asc)
-		@posts.each do |post|
-			@comments = post.post_comments.limit(100).order(:created_at)
-		end
+
+
 
 	end
 end
